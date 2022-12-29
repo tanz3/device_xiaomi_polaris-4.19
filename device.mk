1d6981f0fff57f14a6ed890b1d67ba72eb9c78eb
tanz3 <3222839916@qq.com>
2022-12-29 12:07:35 +0800

    polaris: Move libnfc-nci.conf to /vendor
    Signed-off-by: tanz3 <3222839916@qq.com>

--- device.mk
+++ device.mk
@@ -97,7 +97,7 @@ PRODUCT_PACKAGES += \
     Tag
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_PRODUCT)/etc/libnfc-nci.conf \
+    $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 # Secure element
